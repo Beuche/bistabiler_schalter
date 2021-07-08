ff3fad7fade8a729bad93430c4c1b53eaebad583
Beuche <76558020+Beuche@users.noreply.github.com>
2021-07-08 15:32:23 +0200

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-a=True
 led_an=False
 while True:
     if input.button_is_pressed(Button.A):
         if led_an==True:
             basic.turn_rgb_led_off()
-            led_an=False
         else:
             basic.set_led_color(0x550000)
-            led_an=True
-    while (input.button_is_pressed(Button.A)):
-        a=True
+        led_an=not(led_an)
+        while (input.button_is_pressed(Button.A)):
+            pass
+    if input.button_is_pressed(Button.B):
+        if led_an==True:
+            basic.turn_rgb_led_off()
+        else:
+            basic.set_led_color(0x005500)
+        led_an=not(led_an)
+        while (input.button_is_pressed(Button.B)):
+            pass
+
+
+
+
--- main.ts
+++ main.ts
@@ -1,18 +1,29 @@
-let a = true
 let led_an = false
 while (true) {
     if (input.buttonIsPressed(Button.A)) {
         if (led_an == true) {
             basic.turnRgbLedOff()
-            led_an = false
         } else {
             basic.setLedColor(0x550000)
-            led_an = true
         }
         
+        led_an = !led_an
+        while (input.buttonIsPressed(Button.A)) {
+            
+        }
     }
     
-    while (input.buttonIsPressed(Button.A)) {
-        a = true
+    if (input.buttonIsPressed(Button.B)) {
+        if (led_an == true) {
+            basic.turnRgbLedOff()
+        } else {
+            basic.setLedColor(0x005500)
+        }
+        
+        led_an = !led_an
+        while (input.buttonIsPressed(Button.B)) {
+            
+        }
     }
+    
 }
